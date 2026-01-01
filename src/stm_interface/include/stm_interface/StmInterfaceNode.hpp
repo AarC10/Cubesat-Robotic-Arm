23e8e0c229f594d3c02831fa53a24a2941309a00
Aaron Chan <aarchan108@gmail.com>
2025-12-31 19:29:09 -0500

    Forgot to add these files to git :P

---
+++ src/stm_interface/include/stm_interface/StmInterfaceNode.hpp
@@ -0,0 +1,23 @@
+#include <linux/spi/spidev.h>
+
+#include <rclcpp/rclcpp.hpp>
+
+class StmInterfaceNode : public rclcpp::Node {
+   public:
+    StmInterfaceNode(const char* spiDevice, uint8_t mode = SPI_MODE_0,
+                     uint8_t bitsPerWord = 8, uint32_t speed = 1000000);
+
+    ~StmInterfaceNode() override;
+    
+   private:
+    int spiDevFd;
+    uint8_t mode;
+    uint8_t bitsPerWord;
+    uint32_t speed;
+
+    static constexpr size_t BUFFER_SIZE = 256;
+    uint8_t txBuffer[BUFFER_SIZE];
+    uint8_t rxBuffer[BUFFER_SIZE];
+
+    void performSpiTransfer(const uint8_t* txData, uint8_t* rxData, size_t length);
+};
---
+++ src/stm_interface/src/StmInterfaceNode.cpp
@@ -0,0 +1,59 @@
+#include "stm_interface/StmInterfaceNode.hpp"
+
+#include <fcntl.h>
+#include <linux/spi/spidev.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+StmInterfaceNode::StmInterfaceNode(const char* spiDevice,
+                                   uint8_t mode,
+                                   uint8_t bitsPerWord,
+                                   uint32_t speed)
+    : Node("stm_interface"),
+      mode(mode),
+      bitsPerWord(bitsPerWord),
+      speed(speed) {
+    spiDevFd = open(spiDevice, O_RDWR);
+    if (spiDevFd < 0) {
+        RCLCPP_ERROR(this->get_logger(), "Failed to open SPI device: %s",
+                     spiDevice);
+        throw std::runtime_error("Failed to open SPI device");
+    }
+
+    ioctl(spiDevFd, SPI_IOC_WR_MODE, &mode);
+    ioctl(spiDevFd, SPI_IOC_WR_BITS_PER_WORD, &bitsPerWord);
+    ioctl(spiDevFd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+
+    RCLCPP_INFO(this->get_logger(), "STM Interface Node started.");
+}
+
+StmInterfaceNode::~StmInterfaceNode() {
+    if (spiDevFd >= 0) {
+        close(spiDevFd);
+    }
+
+    RCLCPP_INFO(this->get_logger(), "STM Interface Node shutting down.");
+}
+
+void StmInterfaceNode::performSpiTransfer(const uint8_t* txData,
+                                          uint8_t* rxData, size_t length) {
+    if (length > BUFFER_SIZE) {
+        RCLCPP_ERROR(this->get_logger(),
+                     "Transfer length exceeds buffer size.");
+        throw std::runtime_error("Transfer length exceeds buffer size");
+    }
+
+    struct spi_ioc_transfer tr;
+    tr.tx_buf = reinterpret_cast<unsigned long>(txData);
+    tr.rx_buf = reinterpret_cast<unsigned long>(rxData);
+    tr.len = static_cast<uint32_t>(length);
+    tr.speed_hz = speed;
+
+
+    int ret = ioctl(spiDevFd, SPI_IOC_MESSAGE(1), &tr);
+    if (ret < 1) {
+        RCLCPP_ERROR(this->get_logger(), "Failed to perform SPI transfer.");
+    }
+
+    // TODO: Handle receieved data
+}
