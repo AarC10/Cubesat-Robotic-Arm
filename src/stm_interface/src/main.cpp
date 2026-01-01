0bf19e8a2a89281cfe998ee808574644dc60b518
Aaron Chan <aarchan108@gmail.com>
2025-12-31 19:10:23 -0500

    Fix compile

--- src/stm_interface/src/main.cpp
+++ src/stm_interface/src/main.cpp
@@ -1,14 +1,8 @@
-#include <rclcpp/rclcpp.hpp>
-#include <fcntl.h>
-#include <ioctl.h>
-#include <spi.h>
-#include <spidev.h>
-
 #include "stm_interface/StmInterfaceNode.hpp"
 
 int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<StmInterfaceNode>();
+    auto node = std::make_shared<StmInterfaceNode>("/dev/spidev0.0");
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
