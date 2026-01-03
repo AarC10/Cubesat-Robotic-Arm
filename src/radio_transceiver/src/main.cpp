75fea416156b15e84e919dde6420be57dcb68961
Aaron Chan <aarchan108@gmail.com>
2026-01-03 17:00:39 -0500

    radio transceiver node start code. Some conversion and calc functions, ctor radio config

--- src/radio_transceiver/src/main.cpp
+++ src/radio_transceiver/src/main.cpp
@@ -1,8 +1,10 @@
 #include <rclcpp/rclcpp.hpp>
 
+#include "radio_transceiver/RadioTransceiverNode.hpp"
+
 int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<rclcpp::Node>("radio_transceiver");
+    auto node = std::make_shared<RadioTransceiverNode>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
