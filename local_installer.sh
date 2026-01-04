10d3f2a64e43006b2e94c3cf38ef84c4e9326621
Aaron Chan <aarchan108@gmail.com>
2026-01-03 19:36:56 -0500

    gpg meme

--- local_installer.sh
+++ local_installer.sh
@@ -12,15 +12,15 @@ apt_update() {
 
 # Repos and base prep
 apt_update
-sudo apt-get install -y --no-install-recommends software-properties-common
+sudo apt-get install -y --no-install-recommends ca-certificates curl gnupg lsb-release software-properties-common
 sudo add-apt-repository -y universe
 sudo mkdir -p /etc/apt/keyrings
-curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | sudo tee /etc/apt/keyrings/ros-archive-keyring.asc >/dev/null
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/ros-archive-keyring.asc] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list >/dev/null
+curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | sudo gpg --dearmor -o /etc/apt/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list >/dev/null
 apt_update
 sudo apt-get upgrade -y
 sudo apt-get install -y --no-install-recommends \
-  ca-certificates curl gnupg lsb-release locales sudo gosu procps \
+  locales sudo gosu procps \
   build-essential git wget ninja-build clang clang-format clang-tidy \
   python3-pip python3-rosdep python3-colcon-common-extensions python3-vcstool python3-argcomplete \
   gstreamer1.0-tools gstreamer1.0-plugins-base gstreamer1.0-plugins-good \
