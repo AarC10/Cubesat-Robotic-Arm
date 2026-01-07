a23030fe69ed337c5d27d1d220a2d951a923d324
Aaron Chan <aarchan108@gmail.com>
2026-01-07 00:51:20 +0000

    Send nans if no sensors found

--- src/sensor_reader/src/SensorReaderNode.cpp
+++ src/sensor_reader/src/SensorReaderNode.cpp
@@ -3,12 +3,18 @@
 SensorReaderNode::SensorReaderNode()
     : Node("sensor_reader_node"),
       readIntervalMs(this->declare_parameter<int>("read_interval_ms", 1000)),
-      adxl375Topic(this->declare_parameter<std::string>("adxl375_topic", "/sensor_msgs/msg/Imu")),
-      adxl375I2cDev(this->declare_parameter<std::string>("adxl375_i2c_dev", "/dev/i2c-1")),
-      adxl375Addr(static_cast<uint8_t>(this->declare_parameter<int>("adxl375_addr", 0x53))),
-      ina260Topic(this->declare_parameter<std::string>("ina260_topic", "/sensor_msgs/msg/BatteryState")),
-      ina260I2cDev(this->declare_parameter<std::string>("ina260_i2c_dev", "/dev/i2c-1")),
-      ina260Addr(static_cast<uint8_t>(this->declare_parameter<int>("ina260_addr", 0x40))),
+      adxl375Topic(this->declare_parameter<std::string>(
+          "adxl375_topic", "/sensor_msgs/msg/Imu")),
+      adxl375I2cDev(this->declare_parameter<std::string>("adxl375_i2c_dev",
+                                                         "/dev/i2c-1")),
+      adxl375Addr(static_cast<uint8_t>(
+          this->declare_parameter<int>("adxl375_addr", 0x53))),
+      ina260Topic(this->declare_parameter<std::string>(
+          "ina260_topic", "/sensor_msgs/msg/BatteryState")),
+      ina260I2cDev(
+          this->declare_parameter<std::string>("ina260_i2c_dev", "/dev/i2c-1")),
+      ina260Addr(static_cast<uint8_t>(
+          this->declare_parameter<int>("ina260_addr", 0x40))),
       adxl375(adxl375I2cDev, adxl375Addr),
       ina260(ina260I2cDev, ina260Addr, Ina260::Config{}) {
   adxl375Publisher =
@@ -20,65 +26,69 @@ SensorReaderNode::SensorReaderNode()
       std::chrono::milliseconds(readIntervalMs),
       std::bind(&SensorReaderNode::timeoutCallback, this));
 
-    adxl375Initialized = adxl375.init();
-    ina260Initialized = ina260.init();
+  adxl375Initialized = adxl375.init();
+  ina260Initialized = ina260.init();
 
-    if (adxl375Initialized) {
-        RCLCPP_INFO(this->get_logger(), "ADXL375 initialized successfully.");
-    } else {
-        RCLCPP_ERROR(this->get_logger(), "Failed to initialize ADXL375.");
-    }
+  if (adxl375Initialized) {
+    RCLCPP_INFO(this->get_logger(), "ADXL375 initialized successfully.");
+  } else {
+    RCLCPP_ERROR(this->get_logger(), "Failed to initialize ADXL375.");
+  }
 
-    if (ina260Initialized) {
-        RCLCPP_INFO(this->get_logger(), "INA260 initialized successfully.");
-    } else {
-        RCLCPP_ERROR(this->get_logger(), "Failed to initialize INA260.");
-    }
+  if (ina260Initialized) {
+    RCLCPP_INFO(this->get_logger(), "INA260 initialized successfully.");
+  } else {
+    RCLCPP_ERROR(this->get_logger(), "Failed to initialize INA260.");
+  }
 }
 
 SensorReaderNode::~SensorReaderNode() = default;
 
 void SensorReaderNode::timeoutCallback() {
-    if (adxl375Initialized) {
-        auto accelMS2 = adxl375.readMS2();
-        auto imuMsg = sensor_msgs::msg::Imu();
-        imuMsg.header.stamp = this->now();
-        imuMsg.header.frame_id = "adxl375";
+  auto imuMsg = sensor_msgs::msg::Imu();
+  auto batteryMsg = sensor_msgs::msg::BatteryState();
 
-        if (accelMS2.has_value()) {
-            
-            imuMsg.linear_acceleration.x = accelMS2->x;
-            imuMsg.linear_acceleration.y = accelMS2->y;
-            imuMsg.linear_acceleration.z = accelMS2->z;
-        } else {
-            RCLCPP_ERROR(this->get_logger(), "Failed to read data from ADXL375.");
-            imuMsg.linear_acceleration.x = NAN;
-            imuMsg.linear_acceleration.y = NAN;
-            imuMsg.linear_acceleration.z = NAN;
-        }
+  if (adxl375Initialized) {
+    auto accelMS2 = adxl375.readMS2();
+    imuMsg.header.stamp = this->now();
+    imuMsg.header.frame_id = "adxl375";
 
-        adxl375Publisher->publish(imuMsg);
+    if (accelMS2.has_value()) {
+
+      imuMsg.linear_acceleration.x = accelMS2->x;
+      imuMsg.linear_acceleration.y = accelMS2->y;
+      imuMsg.linear_acceleration.z = accelMS2->z;
+    } else {
+      RCLCPP_ERROR(this->get_logger(), "Failed to read data from ADXL375.");
+      imuMsg.linear_acceleration.x = NAN;
+      imuMsg.linear_acceleration.y = NAN;
+      imuMsg.linear_acceleration.z = NAN;
     }
+  }
 
-    if (ina260Initialized) {
-        auto voltageOpt = ina260.readBusVoltage_V();
-        auto currentOpt = ina260.readCurrent_A();
+  adxl375Publisher->publish(imuMsg);
 
-        auto batteryMsg = sensor_msgs::msg::BatteryState();
-        batteryMsg.header.stamp = this->now();
-        batteryMsg.header.frame_id = "ina260";
+  if (ina260Initialized) {
+    auto voltageOpt = ina260.readBusVoltage_V();
+    auto currentOpt = ina260.readCurrent_A();
 
-        if (voltageOpt.has_value() && currentOpt.has_value()) {
-            batteryMsg.header.stamp = this->now();
-            batteryMsg.voltage = static_cast<float>(voltageOpt.value());
-            batteryMsg.current = static_cast<float>(currentOpt.value());
-            ina260Publisher->publish(batteryMsg);
-        } else {
-            RCLCPP_ERROR(this->get_logger(), "Failed to read data from INA260.");
-            batteryMsg.voltage = NAN;
-            batteryMsg.current = NAN;
-        }
-        ina260Publisher->publish(batteryMsg);
+    batteryMsg.header.stamp = this->now();
+    batteryMsg.header.frame_id = "ina260";
+
+    if (voltageOpt.has_value() && currentOpt.has_value()) {
+      batteryMsg.header.stamp = this->now();
+      batteryMsg.voltage = static_cast<float>(voltageOpt.value());
+      batteryMsg.current = static_cast<float>(currentOpt.value());
+      ina260Publisher->publish(batteryMsg);
+    } else {
+      RCLCPP_ERROR(this->get_logger(), "Failed to read data from INA260.");
+      batteryMsg.voltage = NAN;
+      batteryMsg.current = NAN;
     }
+  } else {
+    batteryMsg.voltage = NAN;
+    batteryMsg.current = NAN;
+  }
+
+  ina260Publisher->publish(batteryMsg);
 }
-
