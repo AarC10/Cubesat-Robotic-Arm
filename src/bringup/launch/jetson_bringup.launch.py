268491e5d8fae6cc9533e86cc1695c2aca6d3983
Aaron Chan <aarchan108@gmail.com>
2026-01-07 00:46:15 +0000

    jetson bringup gscam fix added

--- src/bringup/launch/jetson_bringup.launch.py
+++ src/bringup/launch/jetson_bringup.launch.py
@@ -5,19 +5,20 @@ from launch_ros.actions import Node
 def generate_launch_description() -> LaunchDescription:
     return LaunchDescription([
         Node(
-            package="gscam2",
-            executable="gscam_main",
+            package="gscam",
+            executable="gscam_node",
             name="camera",
             output="screen",
-            parameters=[
-                {
-                    "gscam_config": (
-                        "nvarguscamerasrc sensor-id=0 ! "
-                        "video/x-raw(memory:NVMM),width=1280,height=720,framerate=30/1 ! "
-                        "nvvidconv ! video/x-raw,format=BGR ! appsink"
-                    )
-                }
-            ],
+            parameters=[{
+                "gscam_config": (
+                    "nvarguscamerasrc sensor-id=0 ! "
+                    "video/x-raw(memory:NVMM),width=1280,height=720,framerate=30/1 ! "
+                    "nvvidconv ! "
+                    "video/x-raw,format=BGRx ! "
+                    "videoconvert ! "
+                    "video/x-raw,format=BGR"
+                )
+            }],
         ),
 
         Node(
