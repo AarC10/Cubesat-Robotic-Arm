adea57f9a0e9269deee8a85d5b479bd0052ecb9e
Aaron Chan <aarchan108@gmail.com>
2026-01-06 19:40:15 -0500

    Fix cam stream

--- src/image_handler/launch/camera.launch.py
+++ src/image_handler/launch/camera.launch.py
@@ -1,4 +1,3 @@
-# camera_unified.launch.py
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition
@@ -13,8 +12,9 @@ def generate_launch_description() -> LaunchDescription:
         DeclareLaunchArgument(
             "platform",
             default_value="jetson",
-            description="Target platform: 'jetson' or 'pi'",
+            description="Target platform selector: 'jetson' or 'pi'.",
         ),
+
         Node(
             condition=IfCondition(PythonExpression(["'", platform, "' == 'jetson'"])),
             package="gscam",
@@ -23,19 +23,16 @@ def generate_launch_description() -> LaunchDescription:
             output="screen",
             parameters=[{
                 "gscam_config": (
-                "nvarguscamerasrc sensor-id=0 ! "
-                "video/x-raw(memory:NVMM),width=1280,height=720,framerate=30/1 ! "
-                "nvvidconv ! "
-                "video/x-raw,format=BGRx ! "
-                "videoconvert ! "
-                "video/x-raw,format=BGR ! "
-                "appsink"
+                    "nvarguscamerasrc sensor-id=0 ! "
+                    "video/x-raw(memory:NVMM),width=1280,height=720,framerate=30/1 ! "
+                    "nvvidconv ! "
+                    "video/x-raw,format=BGRx ! "
+                    "videoconvert ! "
+                    "video/x-raw,format=BGR"
                 )
             }],
-           remappings=[
-                ("image", "image_raw"),
-            ],
         ),
+
         Node(
             condition=IfCondition(PythonExpression(["'", platform, "' == 'pi'"])),
             package="camera_ros",
@@ -50,3 +47,4 @@ def generate_launch_description() -> LaunchDescription:
             }],
         ),
     ])
+
